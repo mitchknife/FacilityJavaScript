mitchknife/FacilityJavaScript

commit 265ace379b46a457c9c1b13c6d32ec8528e32b33

Support DTO required fields as not optional properties on interface in TS. (#69)
BREAKING CHANGE: Generate DTO required fields as NOT optional properties on interface in TS. Previously, all DTO fields (including required ones) were optional properties on an interface in TS.

- Implement minimum support for #34. Does not add other validation.
- Update test added in #68 since made change.
- Run codegen since FSD file has required fields.
- Update version and release notes. 

Learn more:
- FSD docs on "Required Fields": https://facilityapi.github.io/define/fsd#fields
- FSD docs on DTOs: https://facilityapi.github.io/define/fsd#data-transfer-objects
- TS docs on optional properties: https://www.typescriptlang.org/docs/handbook/2/objects.html#optional-properties

---------

Co-authored-by: Michael Baldwin <michael.baldwin@faithlife.com>

conformance/src/fastify/conformanceApiPlugin.ts
@@ -905,7 +905,7 @@ export interface ICreateWidgetResponse {
 /** Request for GetWidget. */
 export interface IGetWidgetRequest {
   /** The widget ID. */
-  id?: number;
+  id: number;
 
   /** Don't get the widget if it has this ETag. */
   ifNotETag?: string;
@@ -944,7 +944,7 @@ export interface IDeleteWidgetResponse {
 /** Request for GetWidgetBatch. */
 export interface IGetWidgetBatchRequest {
   /** The IDs of the widgets to return. */
-  ids?: number[];
+  ids: number[];
 }
 
 /** Response for GetWidgetBatch. */
@@ -1075,9 +1075,9 @@ export interface IMixedResponse {
 
 /** Request for Required. */
 export interface IRequiredRequest {
-  query?: string;
+  query: string;
 
-  normal?: string;
+  normal: string;
 
   widget?: IWidget;
 
@@ -1098,7 +1098,7 @@ export interface IRequiredRequest {
 
 /** Response for Required. */
 export interface IRequiredResponse {
-  normal?: string;
+  normal: string;
 }
 
 /** Request for MirrorBytes. */
@@ -1145,7 +1145,7 @@ export interface IWidget {
   id?: number;
 
   /** The name of the widget. */
-  name?: string;
+  name: string;
 }
 
 export interface IAny {

src/Facility.CodeGen.JavaScript/JavaScriptGenerator.cs
@@ -930,7 +930,7 @@ namespace Facility.CodeGen.JavaScript
 				{
 					code.WriteLineSkipOnce();
 					WriteJsDoc(code, fieldInfo);
-					code.WriteLine($"{fieldInfo.Name}?: {RenderFieldType(service.GetFieldType(fieldInfo)!)};");
+					code.WriteLine($"{fieldInfo.Name}{(fieldInfo.IsRequired ? "" : "?")}: {RenderFieldType(service.GetFieldType(fieldInfo)!)};");
 				}
 			}
 		}

Directory.Build.props
@@ -1,7 +1,7 @@
 <Project>
 
   <PropertyGroup>
-    <VersionPrefix>2.11.1</VersionPrefix>
+    <VersionPrefix>3.0.0</VersionPrefix>
     <PackageValidationBaselineVersion>2.8.0</PackageValidationBaselineVersion>
     <LangVersion>12.0</LangVersion>
     <Nullable>enable</Nullable>

conformance/src/conformanceApiTypes.ts
@@ -88,7 +88,7 @@ export interface ICreateWidgetResponse {
 /** Request for GetWidget. */
 export interface IGetWidgetRequest {
   /** The widget ID. */
-  id?: number;
+  id: number;
 
   /** Don't get the widget if it has this ETag. */
   ifNotETag?: string;
@@ -127,7 +127,7 @@ export interface IDeleteWidgetResponse {
 /** Request for GetWidgetBatch. */
 export interface IGetWidgetBatchRequest {
   /** The IDs of the widgets to return. */
-  ids?: number[];
+  ids: number[];
 }
 
 /** Response for GetWidgetBatch. */
@@ -258,9 +258,9 @@ export interface IMixedResponse {
 
 /** Request for Required. */
 export interface IRequiredRequest {
-  query?: string;
+  query: string;
 
-  normal?: string;
+  normal: string;
 
   widget?: IWidget;
 
@@ -281,7 +281,7 @@ export interface IRequiredRequest {
 
 /** Response for Required. */
 export interface IRequiredResponse {
-  normal?: string;
+  normal: string;
 }
 
 /** Request for MirrorBytes. */
@@ -328,7 +328,7 @@ export interface IWidget {
   id?: number;
 
   /** The name of the widget. */
-  name?: string;
+  name: string;
 }
 
 export interface IAny {

tests/Facility.CodeGen.JavaScript.UnitTests/JavaScriptGeneratorTests.cs
@@ -344,8 +344,8 @@ namespace Facility.CodeGen.JavaScript.UnitTests
 
 			var typesFile = result.Files.Single(f => f.Name == "testApiTypes.ts");
 			Assert.That(typesFile.Text, Does.Contain("export interface IWidget {"));
-			Assert.That(typesFile.Text, Does.Contain("id?: string;"));
-			Assert.That(typesFile.Text, Does.Contain("name?: string;"));
+			Assert.That(typesFile.Text, Does.Contain("id: string;"));
+			Assert.That(typesFile.Text, Does.Contain("name: string;"));
 			Assert.That(typesFile.Text, Does.Contain("price?: number;"));
 			Assert.That(typesFile.Text, Does.Contain("}"));
 		}

commit 5a4748dcc6dd4c4634c1b64a8ad925afefec60f1

Fix fastify plugin type errors.
These errors were introduced with https://github.com/FacilityApi/FacilityJavaScript/pull/69

conformance/src/fastify/conformanceApiPlugin.ts
@@ -92,9 +92,9 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IGetApiInfoRequest = {};
+      const request: Partial<IGetApiInfoRequest> = {};
 
-      const result = await api.getApiInfo(request);
+      const result = await api.getApiInfo(request as IGetApiInfoRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -125,12 +125,12 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IGetWidgetsRequest = {};
+      const request: Partial<IGetWidgetsRequest> = {};
 
       const query = req.query as Record<string, string>;
       if (typeof query['q'] === 'string') request.query = query['q'];
 
-      const result = await api.getWidgets(request);
+      const result = await api.getWidgets(request as IGetWidgetsRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -156,11 +156,11 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: ICreateWidgetRequest = {};
+      const request: Partial<ICreateWidgetRequest> = {};
 
       request.widget = req.body as never;
 
-      const result = await api.createWidget(request);
+      const result = await api.createWidget(request as ICreateWidgetRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -192,7 +192,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IGetWidgetRequest = {};
+      const request: Partial<IGetWidgetRequest> = {};
 
       const params = req.params as Record<string, string>;
       if (typeof params['id'] === 'string') request.id = parseInt(params['id'], 10);
@@ -200,7 +200,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       const headers = req.headers as Record<string, string>;
       if (typeof headers['if-none-match'] === 'string') request.ifNotETag = headers['if-none-match'];
 
-      const result = await api.getWidget(request);
+      const result = await api.getWidget(request as IGetWidgetRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -237,7 +237,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IDeleteWidgetRequest = {};
+      const request: Partial<IDeleteWidgetRequest> = {};
 
       const params = req.params as Record<string, string>;
       if (typeof params['id'] === 'string') request.id = parseInt(params['id'], 10);
@@ -245,7 +245,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       const headers = req.headers as Record<string, string>;
       if (typeof headers['if-match'] === 'string') request.ifETag = headers['if-match'];
 
-      const result = await api.deleteWidget(request);
+      const result = await api.deleteWidget(request as IDeleteWidgetRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -281,11 +281,11 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IGetWidgetBatchRequest = {};
+      const request: Partial<IGetWidgetBatchRequest> = {};
 
       request.ids = req.body as never;
 
-      const result = await api.getWidgetBatch(request);
+      const result = await api.getWidgetBatch(request as IGetWidgetBatchRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -319,13 +319,13 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IMirrorFieldsRequest = {};
+      const request: Partial<IMirrorFieldsRequest> = {};
 
       const body = req.body as Record<string, never>;
       request.field = body.field;
       request.matrix = body.matrix;
 
-      const result = await api.mirrorFields(request);
+      const result = await api.mirrorFields(request as IMirrorFieldsRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -351,7 +351,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: ICheckQueryRequest = {};
+      const request: Partial<ICheckQueryRequest> = {};
 
       const query = req.query as Record<string, string>;
       if (typeof query['string'] === 'string') request.string = query['string'];
@@ -363,7 +363,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       if (typeof query['enum'] === 'string') request.enum = query['enum'] as Answer;
       if (typeof query['datetime'] === 'string') request.datetime = query['datetime'];
 
-      const result = await api.checkQuery(request);
+      const result = await api.checkQuery(request as ICheckQueryRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -389,7 +389,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: ICheckPathRequest = {};
+      const request: Partial<ICheckPathRequest> = {};
 
       const params = req.params as Record<string, string>;
       if (typeof params['string'] === 'string') request.string = params['string'];
@@ -401,7 +401,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       if (typeof params['enum'] === 'string') request.enum = params['enum'] as Answer;
       if (typeof params['datetime'] === 'string') request.datetime = params['datetime'];
 
-      const result = await api.checkPath(request);
+      const result = await api.checkPath(request as ICheckPathRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -427,7 +427,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IMirrorHeadersRequest = {};
+      const request: Partial<IMirrorHeadersRequest> = {};
 
       const headers = req.headers as Record<string, string>;
       if (typeof headers['string'] === 'string') request.string = headers['string'];
@@ -439,7 +439,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       if (typeof headers['enum'] === 'string') request.enum = headers['enum'] as Answer;
       if (typeof headers['datetime'] === 'string') request.datetime = headers['datetime'];
 
-      const result = await api.mirrorHeaders(request);
+      const result = await api.mirrorHeaders(request as IMirrorHeadersRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -481,7 +481,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IMixedRequest = {};
+      const request: Partial<IMixedRequest> = {};
 
       const params = req.params as Record<string, string>;
       if (typeof params['path'] === 'string') request.path = params['path'];
@@ -495,7 +495,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       const body = req.body as Record<string, never>;
       request.normal = body.normal;
 
-      const result = await api.mixed(request);
+      const result = await api.mixed(request as IMixedRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -538,7 +538,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IRequiredRequest = {};
+      const request: Partial<IRequiredRequest> = {};
 
       const query = req.query as Record<string, string>;
       if (typeof query['query'] === 'string') request.query = query['query'];
@@ -554,7 +554,7 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       request.hasWidget = body.hasWidget;
       request.point = body.point;
 
-      const result = await api.required(request);
+      const result = await api.required(request as IRequiredRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -580,14 +580,14 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IMirrorBytesRequest = {};
+      const request: Partial<IMirrorBytesRequest> = {};
 
       const headers = req.headers as Record<string, string>;
       if (typeof headers['content-type'] === 'string') request.type = headers['content-type'];
 
       request.content = req.body as never;
 
-      const result = await api.mirrorBytes(request);
+      const result = await api.mirrorBytes(request as IMirrorBytesRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -617,14 +617,14 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IMirrorTextRequest = {};
+      const request: Partial<IMirrorTextRequest> = {};
 
       const headers = req.headers as Record<string, string>;
       if (typeof headers['content-type'] === 'string') request.type = headers['content-type'];
 
       request.content = req.body as never;
 
-      const result = await api.mirrorText(request);
+      const result = await api.mirrorText(request as IMirrorTextRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];
@@ -654,11 +654,11 @@ export const conformanceApiPlugin: FastifyPluginAsync<ConformanceApiPluginOption
       },
     },
     handler: async function (req, res) {
-      const request: IBodyTypesRequest = {};
+      const request: Partial<IBodyTypesRequest> = {};
 
       request.content = req.body as never;
 
-      const result = await api.bodyTypes(request);
+      const result = await api.bodyTypes(request as IBodyTypesRequest);
 
       if (result.error) {
         const status = result.error.code && standardErrorCodes[result.error.code];

src/Facility.CodeGen.JavaScript/JavaScriptGenerator.cs
@@ -710,7 +710,7 @@ namespace Facility.CodeGen.JavaScript
 							}
 							using (code.Block("handler: async function (req, res) {", "}"))
 							{
-								code.WriteLine("const request" + IfTypeScript($": I{capMethodName}Request") + " = {};");
+								code.WriteLine("const request" + IfTypeScript($": Partial<I{capMethodName}Request>") + " = {};");
 								if (httpMethodInfo.PathFields.Count != 0)
 								{
 									code.WriteLine();
@@ -756,7 +756,7 @@ namespace Facility.CodeGen.JavaScript
 								}
 
 								code.WriteLine();
-								code.WriteLine($"const result = await api.{methodName}(request);");
+								code.WriteLine($"const result = await api.{methodName}(request{IfTypeScript($" as I{capMethodName}Request")});");
 
 								code.WriteLine();
 								using (code.Block("if (result.error) {", "}"))